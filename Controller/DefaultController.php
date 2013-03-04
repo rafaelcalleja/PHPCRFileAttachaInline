33efe20c8ac265a1d525dc4ef845e255aa62353b
Rafael Calleja <rafa.calleja@d-noise.net>
2013-03-04 16:32:09 +0100

    [fix]bundle structure

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('FileAttachInlineBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('RCPHPCRFileAttachInlineBundle:Default:index.html.twig', array('name' => $name));
     }
 }
--- RCPHPCRFileAttachInlineBundle.php
+++ RCPHPCRFileAttachInlineBundle.php
@@ -4,6 +4,6 @@ namespace RC\PHPCR\FileAttachInlineBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class FileAttachInlineBundle extends Bundle
+class RCPHPCRFileAttachInlineBundle extends Bundle
 {
 }
